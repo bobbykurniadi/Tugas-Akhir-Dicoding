aaf19617f24555ff5b67ff4f314918aec9e5f69a
bobbykurniadi <74778248+bobbykurniadi@users.noreply.github.com>
2022-08-10 19:59:04 +0700

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+console.log("JavaScript berhasil digunakan");
+
+//edited profile
+document.getElementById('name').innerHTML = 'Bobby Kurniadi';
+document.getElementById('date').innerHTML = '06 Mei 1999';
+document.getElementById('age').innerHTML = '22 Tahun';
+document.getElementById('city').innerHTML = 'DKI Jakarta';
+document.getElementById('religion').innerHTML = 'Islam';
+document.getElementById('graduate').innerHTML = 'UPN VETERAN JAKARTA';
